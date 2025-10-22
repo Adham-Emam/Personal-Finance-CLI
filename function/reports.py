1e5a424378f0adbed8d49d31487603fb576e4e6d
youssef-M-hassan <youssef.m.hassan56@gmail.com>
2025-10-22 05:13:12 +0300

    tweaked monthly report and added category summary

--- function/reports.py
+++ function/reports.py
@@ -26,24 +26,53 @@ class Reports:
         with open(self.file_path, "r", newline="") as csvfile:
             reader = csv.DictReader(csvfile)
             for row in reader:
-                row["amount"] = int(row["amount"])
+                row["amount"] = float(row["amount"])
                 row["date"] = datetime.strptime(row["date"], "%Y-%m-%d").date()
+                row["category"] = row["category"].strip().lower()
                 transactions.append(row)
         return transactions
-    def monthly_summary(self, year, month):
+    def monthly_summary(self):
         """monthly report"""
         transactions = self.read_transactions()
         income_total=0
         expenses_total=0
         now=datetime.now()
         last_month=now.month-1 if now.month>1 else 12
-        last_year=now.year if now.month>1 else now.year-1
-        for tx in transactions:
-            if tx["date"].year == year and tx["date"].month == month:
-                if tx["type"] =="income":
-                    income_total += tx["amount"]
-                elif tx["type"] =="expense":
-                    expenses_total += tx["amount"]
-        return {"income_total": income_total, "expenses_total": expenses_total, "net_total": income_total - expenses_total}
+        last_month_year=now.year if now.month>1 else now.year-1
+        for row in transactions:
+            date=row["date"]
+            if date.month==last_month and date.year==last_month_year:
+                amount=row["amount"]
+                if row["type"]=="income":
+                    income_total+=amount
+                elif row["type"]=="expense":
+                    expenses_total+=amount
+            net_total=income_total-expenses_total
+        print(f"Monthly Summary for {last_month}/{last_month_year}:")
+        print(f"Total income: ${income_total:.2f}")
+        print(f"Total expenses: ${expenses_total:.2f}")
+        print(f"Net total: ${net_total:.2f}")
+        return income_total, expenses_total, net_total
+    def category_summary(self):
+        """category report"""
+        transactions=self.read_transactions()
+        category=input("Enter your desired category: ").strip().lower()
+        total=0
+        for row in transactions:
+            if row["category"]== category:
+                amount=row["amount"]
+                total+=amount
+        if total ==0:
+            print(f"no transactions were found for {category} category")
+        else:
+            print(f"Total for category: {category} is : ${total:.2f}")
+                    
+        # for tx in transactions:
+        #     if tx["date"].year == year and tx["date"].month == month:
+        #         if tx["type"] =="income":
+        #             income_total += tx["amount"]
+        #         elif tx["type"] =="expense":
+        #             expenses_total += tx["amount"]
+        # return {"income_total": income_total, "expenses_total": expenses_total, "net_total": income_total - expenses_total}
     
     
